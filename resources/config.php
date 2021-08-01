fd75b718f6036e42c82e9b5a4ff0c924722826c3
Salim66 <salimhasanriad8@gmail.com>
2021-08-01 18:56:57 +0600

    Deifining paths with constants

---
+++ resources/config.php
@@ -0,0 +1,17 @@
+<?php 
+
+    // Create site path
+    defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
+    defined("TEMPLATE_FRONT") ? null : define("TEMPLATE_FRONT", __DIR__ . DS . 'templates\front');
+    defined("TEMPLATE_BACK") ? null : define("TEMPLATE_BACK", __DIR__ . DS . 'templates\back');
+
+
+    echo TEMPLATE_FRONT; echo"<br>";
+    echo TEMPLATE_BACK;
+
+    
+
+
+
+
+?>
