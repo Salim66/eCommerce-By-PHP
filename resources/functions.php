ed04c238a27f9f355de6dc5764052df10c84c881
Salim66 <salimhasanriad8@gmail.com>
2021-08-02 11:21:36 +0600

    Useful Custom Helper Functions

--- resources/functions.php
+++ resources/functions.php
@@ -1,6 +1,50 @@
 <?php
 
+/**
+ * Create redirect function
+ */
+function redirect($loaction){
+    header("location: $loaction");
+}
 
+
+/**
+ * Crearte query function
+ */
+function query($sql){
+    global $connection;
+    return mysqli_query($connection, $sql);
+}
+
+
+/**
+ * Create database conection confirm failed function
+ */
+function confirm($result){
+    global $connection;
+
+    if(!$result){
+        die("Query Failed " . mysqli_error($connection));
+    }
+
+}
+
+
+/**
+ * Create escape String function
+ */
+function escapeString($string){
+    global $connection;
+    return mysqli_real_escape_string($connection, $string);
+}
+
+
+/**
+ * Create fetch array function 
+ */
+function fetchArray($result){
+    return mysqli_fetch_array($result);
+}
 
 
 
