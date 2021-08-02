7548d0212119bbb642cac7a48277b4827dc92758
Salim66 <salimhasanriad8@gmail.com>
2021-08-02 12:09:21 +0600

    Creating the Products Table and Data Insertion

--- public/index.php
+++ public/index.php
@@ -28,6 +28,14 @@
 
                 <div class="row">
 
+
+                    <?php
+                    
+                    
+                    
+                    
+                    ?>
+
                     <div class="col-sm-4 col-lg-4 col-md-4">
                         <div class="thumbnail">
                             <img src="http://placehold.it/320x150" alt="">
