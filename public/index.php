702462f11b20409862e51341706dc45c5fbf035a
Salim66 <salimhasanriad8@gmail.com>
2021-08-02 09:18:37 +0600

    Create database ecom_db and test successfully connected or not

--- public/index.php
+++ public/index.php
@@ -1,3 +1,10 @@
+<?php
+
+    require_once('../resources/config.php');
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
---
+++ resources/functions.php
@@ -0,0 +1,13 @@
+<?php
+
+    if($connection){
+        echo "Connection success";
+    }
+
+    echo "<br>";
+
+    echo "I am from functions.php file";
+
+
+
+?>
