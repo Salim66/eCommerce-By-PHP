4a9f8eff2ba293d706c76dbf7fef0a7a2f5348e7
Salim66 <salimhasanriad8@gmail.com>
2021-08-02 10:57:23 +0600

    Displaying Dynamic Categories

--- resources/templates/front/side_nav.php
+++ resources/templates/front/side_nav.php
@@ -3,16 +3,26 @@
     <div class="list-group">
 
         <?php
-        
-        
+
+            // Create query for all category
+            $sql = "SELECT * FROM categories";
+            // Execute the query
+            $send_query = mysqli_query($connection, $sql);
+
+            if(!$send_query){
+                die("Query Failed " . mysqli_error($connection));
+            }
+
+            
+            // Fetch query data
+            while($row = mysqli_fetch_array($send_query)){
+                echo "<a href='category.html' class='list-group-item'>{$row['cat_title']}</a>";
+            }
         
         
         
         ?>
 
 
-        <a href="category.html" class="list-group-item">Category 1</a>
-        <a href="#" class="list-group-item">Category 2</a>
-        <a href="#" class="list-group-item">Category 3</a>
     </div>
 </div>
