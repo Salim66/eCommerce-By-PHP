db7a557fd3bb0d4d638d76d3270f188a08f39ae0
Salim66 <salimhasanriad8@gmail.com>
2021-08-02 14:29:15 +0600

    Product Button Link to Single Produc page

--- resources/functions.php
+++ resources/functions.php
@@ -60,24 +60,24 @@ function getProducts(){
 
     while($row = fetchArray($query)){
         
-        $product = <<<DELIMETER
+        $product = <<<DELIMITER
 
             <div class="col-sm-4 col-lg-4 col-md-4">
                 <div class="thumbnail">
-                    <img src="http://placehold.it/320x150" alt="">
+                    <a href="item.php?id={$row['product_id']}"><img src="http://placehold.it/320x150" alt=""></a>
                     <div class="caption">
-                        <h4 class="pull-right">{$row['product_price']}</h4>
+                        <h4 class="pull-right">&#36;{$row['product_price']}</h4>
                         <h4><a href="product.html">{$row['product_title']}</a>
                         </h4>
                         <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
                     
-                        <a class="btn btn-primary" target="_blank" href="http://maxoffsky.com/code-blog/laravel-shop-tutorial-1-building-a-review-system/">View Tutorial</a>
+                        <a class="btn btn-primary" target="_self" href="item.php?id={$row['product_id']}">View Tutorial</a>
                     </div>
                     
                 </div>
             </div>
 
-        DELIMETER;
+        DELIMITER;
 
         echo $product;
 
