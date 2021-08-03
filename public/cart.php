93476b7e1711f70aaddea1228772ac08e21fdf37
Salim66 <salimhasanriad8@gmail.com>
2021-08-03 10:47:15 +0600

    Add Product Session Code prt 2

--- public/cart.php
+++ public/cart.php
@@ -6,8 +6,23 @@
     if(isset($_GET['add'])){
 
 
-        $_SESSION['product_' . $_GET['add']] += 1;
-        redirect("index.php");
+        $query = query("SELECT * FROM products WHERE product_id= " .escapeString($_GET['add']). " ");
+        confirm($query);
+
+        while($row = fetchArray($query)){
+
+            if($row['product_quantity'] != $_SESSION['product_'.$_GET['add']]){
+                $_SESSION['product_'.$_GET['add']] += 1;
+            }else {
+                setMessage("We only have ".$row['product_quantity']." available.");
+                redirect("checkout.php");
+            }
+
+        }
+
+
+        // $_SESSION['product_' . $_GET['add']] += 1;
+        // redirect("index.php");
 
 
     }
