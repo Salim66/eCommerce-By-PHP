232cbb4d748376df6244625dcc9ef5a43e0a3e3d
Salim66 <salimhasanriad8@gmail.com>
2021-08-04 17:03:59 +0600

    Paypal form submit problem solved!

--- public/checkout.php
+++ public/checkout.php
@@ -26,7 +26,9 @@
 <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
 <input type="hidden" name="cmd" value="_cart">
 <input type="hidden" name="business" value="sb-5ehm87019775@business.example.com">
+<!-- <input type="hidden" name="business" value="edwindiaz123-facilitator@gmail.com"> -->
 <input type="hidden" name="currency_code" value="USD">
+<input type="hidden" name="upload" value="1">
     <table class="table table-striped">
         <thead>
           <tr>
